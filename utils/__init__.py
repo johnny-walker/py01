dcec56dd892cc808356845cc358a7ad179e6bcc4
johnny-walker <albert_hsu@cyberlink.com>
2021-06-13 11:57:41 +0800

    remove module folder

--- utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .blurs import *  
--- utils/display/imageshow.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt
-
-class Plot:
-    def show_diff(this, img, dst):
-        plt.subplot(121), plt.imshow(img), plt.title('Image 1')
-        plt.xticks([]), plt.yticks([])
-        plt.subplot(122), plt.imshow(dst), plt.title('Image 2')
-        plt.xticks([]), plt.yticks([])
-        plt.show()
