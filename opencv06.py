614ce9532e0d57e7b1a0f9ac351bf755082afda1
johnny-walker <albert_hsu@cyberlink.com>
2021-06-17 11:49:50 +0800

    add argparse

--- opencv06.py
+++ opencv06.py
@@ -1,5 +1,6 @@
 #https://docs.opencv.org/master/da/d6e/tutorial_py_geometric_transformations.html
 
+import argparse
 import os
 import numpy as np
 import cv2
@@ -38,7 +39,7 @@ def show_diff(img, dst):
 
 
 def resize(args):
-    img = imread_RGB(args['src'])
+    img = imread_RGB(args.src)
 
     dst = cv2.resize(img, None, fx=0.25, fy=0.25, interpolation = cv2.INTER_CUBIC)
     ''' 
@@ -51,12 +52,12 @@ def resize(args):
 
 
 def translate(args):
-    img = imread_RGB(args['src'])
+    img = imread_RGB(args.src)
     rows, cols = img.shape[:2]
 
     # [ 1, 0, tx
     #   0, 1, ty ]
-    M = np.float32( [[1, 0, args['tx']], [0, 1, args['ty']]] )
+    M = np.float32( [[1, 0, args.tx], [0, 1, args.ty]] )
     '''
     src – 输入图像。
     M – 变换矩阵。
@@ -68,11 +69,11 @@ def translate(args):
     return dst
 
 
-def rotate(args):
-    if 'img' not in args.keys():
-        img = imread_RGB(args['src'])
+def rotate(args, image=None):
+    if type(image) is np.ndarray:
+        img = image
     else:
-        img = args['img']
+        img = imread_RGB(args.src)
     rows, cols = img.shape[:2]
 
     '''
@@ -81,53 +82,51 @@ def rotate(args):
     scale：旋转后图像相比原来的缩放比例
     M:计算得到的旋转矩阵
     '''
-    M = cv2.getRotationMatrix2D(((cols-1)/2.0, (rows-1)/2.0), args['angle'], args['scale'])
+    M = cv2.getRotationMatrix2D(((cols-1)/2.0, (rows-1)/2.0), args.angle, args.scale)
     dst = cv2.warpAffine(img, M, (cols,rows))
-    if args['show']:
+    if args.show:
         show_diff(img, dst)
         #show_one(dst)
     return dst
 
 
 def animate_rotate(args):
-    img = cv2.imread(args['src'])
-    args['img'] = img
-    args['show'] = False
+    img = cv2.imread(args.src)
+    args.show = False
 
     # 讓視窗可以自由縮放大小
     height, width = img.shape[:2]
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     while True:
-        dst = rotate(args)
+        dst = rotate(args, img)
         cv2.imshow('frame', dst)
         cv2.resizeWindow('frame', (width//2, height//2))
-        args['angle'] +=  1 % 360
+        args.angle +=  1 % 360
         key = cv2.waitKey(33)
         # ESC
         if key == 27:
             break
 
 if __name__ == '__main__':
-    args = {}
-    args['src'] = 'data/dog.jpeg'
-    args['show'] = True
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', default='data/dog.jpeg', help="source file")
+    parser.add_argument('--show', action='store_true', help="source file")
+    parser.add_argument('--tx', type=int, default=200, help='horizontal translation') 
+    parser.add_argument('--ty', type=int, default=150, help='veritcal translation') 
+    parser.add_argument('--angle', type=int, default=10, help='rotation angle') 
+    parser.add_argument('--scale', type=float, default=0.5, help="scale ratio") 
+    parser.add_argument('--output', default='output/output.png', help="output result") 
+    parser.add_argument('--save', default=True, help="save result") 
+    args = parser.parse_args()
+
     img = None
-
     #img = resize(args)
-    
-    '''
-    args['tx'] = 200
-    args['ty'] = 150
     img = translate(args)
-    '''
-
-    #args['show'] = False
-    args['angle'] = 10
-    args['scale'] = 0.5
     #img = rotate(args)
     animate_rotate(args)
 
-    if img is not None:
-        destination = 'output/output.png'
+    if args.save and img is not None:
+        destination = args.output
         check_dir(destination)
         imwrite(destination, img)
+        print("save file under: ", args.output)
